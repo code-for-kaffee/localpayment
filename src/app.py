f9ad7560cc41602e59dab243140740c73dfe3b6e
Federico <fedegomezavalos@gmail.com>
2021-07-30 00:42:10 -0300

    Error handling

--- src/app.py
+++ src/app.py
@@ -17,74 +17,83 @@ version = "api/v1"
 
 @app.route(f'/{version}/transactions', methods=['GET'])
 def get_all_trx():
-    trxs = mongo.db.localpayment_db.find()
-    response = json_util.dumps(trxs)
-    return Response(response, mimetype='application/json')
+    try:
+        trxs = mongo.db.localpayment_db.find()
+        response = json_util.dumps(trxs)
+        return Response(response, mimetype='application/json')
+    except:
+        return server_error()
 
 
 @app.route(f'/{version}/transactions', methods=['POST'])
 def create_new_trx():
-    if not request.json['user'] and request.json['feature'] and request.json['amount']:
-        return bad_request("please check the json")
+    try:
+        if not request.json['user'] and request.json['feature'] and request.json['amount']:
+            return bad_request("please check the json")
 
-    user = request.json['user']
-    feature = request.json['feature']
-    amount = request.json['amount']
+        user = request.json['user']
+        feature = request.json['feature']
+        amount = request.json['amount']
 
-    req = requests.get(
-        f'http://localpayment_node_app_1:3000/{version}/user/{user}')
+        req = requests.get(
+            f'http://localpayment_node_app_1:3000/{version}/user/{user}')
 
-    if req:
-        if feature != 'PAYIN' and feature != 'PAYOUT':
-            return bad_request("only PAYIN or PAYOUT allowed as feature")
-        if user and feature and amount:
-            trx = mongo.db.localpayment_db.insert({
-                'user': user,
-                'feature': feature,
-                'amount': amount
-            })
-            response = {
-                'trx_number': str(trx),
-                'type': feature,
-                'amount': amount,
-                'by': user
-            }
-            return response
+        if req:
+            if feature != 'PAYIN' and feature != 'PAYOUT':
+                return bad_request("only PAYIN or PAYOUT allowed as feature")
+            if user and feature and amount:
+                trx = mongo.db.localpayment_db.insert({
+                    'user': user,
+                    'feature': feature,
+                    'amount': amount
+                })
+                response = {
+                    'trx_number': str(trx),
+                    'type': feature,
+                    'amount': amount,
+                    'by': user
+                }
+                return response
+            else:
+                return bad_request("something is missing, please check the JSON format")
         else:
-            return bad_request("something is missing, please check the JSON format")
-    else:
-        return not_found("user doesn't exist")
+            return not_found("user doesn't exist")
+    except:
+        return server_error()
 
 
 @app.route(f'/{version}/transactions/balance/<user>', methods=['GET'])
 def get_all_trx_by_user(user):
+    try:
+        req = requests.get(
+            f'http://localpayment_node_app_1:3000/{version}/user/{user}')
 
-    req = requests.get(
-        f'http://localpayment_node_app_1:3000/{version}/user/{user}')
-
-    if req:
-        user_data = mongo.db.localpayment_db.find({'user': int(user), })
-        balance = 0
-        for data in user_data:
-            balance += data['amount']
-        print(balance)
-        response = {
-            "user": user,
-            "balance": balance
-        }
-        return response
-    else:
-        return not_found("user doesn't exist")
-
+        if req:
+            user_data = mongo.db.localpayment_db.find({'user': int(user), })
+            balance = 0
+            for data in user_data:
+                balance += data['amount']
+            print(balance)
+            response = {
+                "user": user,
+                "balance": balance
+            }
+            return response
+        else:
+            return not_found("user doesn't exist")
+    except:
+        return server_error()
 
 @app.route(f'/{version}/transactions/<id>', methods=['DELETE'])
 def delete_all_user_records(id):
-    mongo.db.localpayment_db.delete_one({'_id': ObjectId(id)})
-    response = jsonify({'message': 'transaction' +
-                       id + ' Deleted Successfully'})
-    response.status_code = 200
-    return response
-
+    try:
+        mongo.db.localpayment_db.delete_one({'_id': ObjectId(id)})
+        response = jsonify({'message': 'transaction' +
+                        id + ' Deleted Successfully'})
+        response.status_code = 200
+        return response
+    except:
+        return server_error()
 
 @app.errorhandler(404)
 def not_found(text):
@@ -107,6 +116,14 @@ def bad_request(text):
     response.status_code = 400
     return response
 
-
+@app.errorhandler(500)
+def server_error():
+    message = {
+        'message': 'Unkown error, please try again later',
+        'status': 500
+    }
+    response = jsonify(message)
+    response.status_code = 500
+    return response
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
